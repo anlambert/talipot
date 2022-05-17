63a736ecbe34a6af0edd66948c1d548598a86685
Antoine Lambert <antoine.lambert33@gmail.com>
2022-05-17 22:47:38 +0200

    talipot-core/PluginLibraryLoader: Fix build on ArchLinux

--- library/talipot-core/src/PluginLibraryLoader.cpp
+++ library/talipot-core/src/PluginLibraryLoader.cpp
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright (C) 2019-2021  The Talipot developers
+ * Copyright (C) 2019-2022  The Talipot developers
  *
  * Talipot is a fork of Tulip, created by David Auber
  * and the Tulip development Team from LaBRI, University of Bordeaux
@@ -21,6 +21,7 @@
 #include <windows.h>
 #else
 #include <cstdlib>
+#include <cstring>
 #include <dlfcn.h>
 #include <dirent.h>
 #include <sys/stat.h>
