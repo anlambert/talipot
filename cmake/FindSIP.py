fc02415d53c980ad4fa25e66d6fa40bc1bc0731f
Antoine Lambert <antoine.lambert33@gmail.com>
2022-09-02 00:17:07 +0200

    cmake/FindSIP.py: Remove not needed and deprecated instructions

--- cmake/FindSIP.py
+++ cmake/FindSIP.py
@@ -1,5 +1,3 @@
-from distutils.sysconfig import get_python_lib
-
 import sipbuild
 from sipbuild.module import abi_version
 
@@ -7,6 +5,3 @@ print("sip_version:%06.0x" % sipbuild.version.SIP_VERSION)
 print("sip_version_num:%d" % sipbuild.version.SIP_VERSION)
 print("sip_version_str:%s" % sipbuild.version.SIP_VERSION_STR)
 print("sip_abi_version:%s" % abi_version.resolve_abi_version(None) + ".0")
-
-python_modules_dir = get_python_lib(plat_specific=1)
-print("default_sip_dir:%s" % python_modules_dir)
