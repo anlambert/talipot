a762de1afc5d2ffbde89d89ce7496b81c5cdbb93
Antoine Lambert <antoine.lambert33@gmail.com>
2023-05-24 23:33:53 +0200

    bundlers/linux: Exclude Python 3.12 dev version for wheels build

--- bundlers/linux/talipot_python_wheels_manylinux_build.sh
+++ bundlers/linux/talipot_python_wheels_manylinux_build.sh
@@ -71,7 +71,8 @@ fi
 for CPYBIN in /opt/python/cp*/bin
 do
   if [[ $CPYBIN == *"cp27"* ]] || [[ $CPYBIN == *"cp34"* ]] \
-    || [[ $CPYBIN == *"cp35"* ]] || [[ $CPYBIN == *"cp36"* ]]
+    || [[ $CPYBIN == *"cp35"* ]] || [[ $CPYBIN == *"cp36"* ]] \
+    || [[ $CPYBIN == *"cp312"* ]]
   then
     continue
   fi
