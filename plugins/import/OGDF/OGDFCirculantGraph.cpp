ebacf2028233a86ac3554941a844eff2acd561d8
Antoine Lambert <antoine.lambert33@gmail.com>
2023-11-14 22:16:01 +0100

    plugins/import: Wrap OGDF graph generators (WIP)

---
+++ plugins/import/OGDF/OGDFCirculantGraph.cpp
@@ -0,0 +1,56 @@
+/**
+ *
+ * Copyright (C) 2024  The Talipot developers
+ *
+ * Talipot is a fork of Tulip, created by David Auber
+ * and the Tulip development Team from LaBRI, University of Bordeaux
+ *
+ * See the AUTHORS file at the top-level directory of this distribution
+ * License: GNU General Public License version 3, or any later version
+ * See top-level LICENSE file for more information
+ *
+ */
+
+#include <ogdf/basic/graph_generators/deterministic.h>
+
+#include "OGDFImportBase.h"
+
+using namespace std;
+
+static constexpr string_view paramHelp[] = {
+    // n
+    "the number of nodes of the generated graph",
+
+    // jumps
+    "the array of distances for edges to be created",
+};
+
+//=================================================================================
+
+class OGDFCirculantGraph : public OGDFImportBase {
+public:
+  PLUGININFORMATION(
+      "Circulant Graph (OGDF)", "Antoine Lambert", "05/2024",
+      "Generates a simple, undirected graph on n nodes V := v_0,v_1,...,v_{n-1} that contains "
+      "exactly the edges {v_iv_{i+d}; v_i ∈ V, d ∈ jumps} where node indices are to be understood "
+      "modulo n. The order of nodes induced by G is the sequence V previously given.",
+      "1.0", "OGDF")
+
+  OGDFCirculantGraph(tlp::PluginContext *context) : OGDFImportBase(context) {
+    addInParameter<int>("n", paramHelp[0].data(), "100");
+    addInParameter<vector<int>>("jumps", paramHelp[1].data(), "(10, 20, 30, 40)");
+  }
+
+  bool importOGDFGraph() override {
+    int n = 100;
+    vector<int> jumps = {10, 20, 30, 40};
+    if (dataSet != nullptr) {
+      dataSet->get("n", n);
+      dataSet->get("jumps", jumps);
+    }
+    ogdf::circulantGraph(G, n, vectorToOGDFArray(jumps));
+    return true;
+  }
+};
+
+PLUGIN(OGDFCirculantGraph)
---
+++ plugins/import/OGDF/OGDFRandomChungLuGraph.cpp
@@ -0,0 +1,74 @@
+/**
+ *
+ * Copyright (C) 2024  The Talipot developers
+ *
+ * Talipot is a fork of Tulip, created by David Auber
+ * and the Tulip development Team from LaBRI, University of Bordeaux
+ *
+ * See the AUTHORS file at the top-level directory of this distribution
+ * License: GNU General Public License version 3, or any later version
+ * See top-level LICENSE file for more information
+ *
+ */
+
+#include <numeric>
+
+#include <ogdf/basic/graph_generators/randomized.h>
+
+#include "OGDFImportBase.h"
+
+using namespace std;
+
+static constexpr string_view paramHelp[] = {
+    // expectedDegreeDistribution
+    "a list of expected degrees, or weights, for the individual nodes. Its length defines the "
+    "number of nodes n.",
+};
+
+//=================================================================================
+
+class OGDFRandomChungLuGraph : public OGDFImportBase {
+public:
+  PLUGININFORMATION(
+      "Random Chung Lu Graph (OGDF)", "Antoine Lambert", "06/2024",
+      "Creates a graph where edges are inserted based on given weights. Implements the algorithm "
+      "described in: \"The average distance in a random graph with given expected degrees, Fang "
+      "Chung and Linyuan Lu, https://www.math.ucsd.edu/~fan/wp/aveflong.pdf\". Given an expected "
+      "degree distribution of length n: (w_1, ..., w_n) with 0 < w_k < n. Let S be the sum over "
+      "all expected degrees. Consider each edge independently and insert it with probability "
+      "p_{ij} = (w_i * w_j) / S. Each degree must be strictly between 0 and n, and the square of "
+      "the maximal expected degree must be lower than the sum of all expected degrees.",
+      "1.0", "OGDF")
+
+  OGDFRandomChungLuGraph(tlp::PluginContext *context) : OGDFImportBase(context) {
+    addInParameter<vector<int>>("expectedDegreeDistribution", paramHelp[0].data(),
+                                "(1, 2, 2, 3, 3, 3, 4)");
+  }
+
+  bool importOGDFGraph() override {
+    vector<int> expectedDegreeDistribution = {1, 2, 2, 3, 3, 3, 4};
+
+    if (dataSet != nullptr) {
+      dataSet->get("expectedDegreeDistribution", expectedDegreeDistribution);
+    }
+
+    int S = reduce(expectedDegreeDistribution.begin(), expectedDegreeDistribution.end());
+
+    for (auto w : expectedDegreeDistribution) {
+      if (w <= 0 || w >= expectedDegreeDistribution.size()) {
+        pluginProgress->setError("each degree must be strictly between 0 and n");
+        return false;
+      }
+      if (w * w >= S) {
+        pluginProgress->setError("the square of the maximal expected degree must be lower than the "
+                                 "sum of all expected degrees");
+        return false;
+      }
+    }
+
+    ogdf::randomChungLuGraph(G, vectorToOGDFArray(expectedDegreeDistribution));
+    return true;
+  }
+};
+
+PLUGIN(OGDFRandomChungLuGraph)
