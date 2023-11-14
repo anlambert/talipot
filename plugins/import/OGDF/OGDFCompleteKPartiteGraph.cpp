9ebbe104b94d4e08fef1a4a7e4ad45974bb72972
Antoine Lambert <antoine.lambert33@gmail.com>
2023-11-14 22:16:01 +0100

    plugins/import: Wrap OGDF graph generators (WIP)

---
+++ plugins/import/OGDF/OGDFCompleteKPartiteGraph.cpp
@@ -0,0 +1,46 @@
+/**
+ *
+ * Copyright (C) 2024  The Talipot developers
+ *
+ * Talipot is a fork of Tulip, created by David Auber
+ * and the Tulip development Team from LaBRI, University of Bordeaux
+ *
+ * See the AUTHORS file at the top-level directory of this distribution
+ * License: GNU General Public License version 3, or any later version
+ * See top-level LICENSE file for more information
+ *
+ */
+
+#include <ogdf/basic/graph_generators/deterministic.h>
+
+#include "OGDFImportBase.h"
+
+using namespace std;
+
+static constexpr string_view paramHelp[] = {
+    // signature
+    "contains the positive values k1, k2, ..., kn.",
+};
+
+//=================================================================================
+
+class OGDFCompleteKPartiteGraph : public OGDFImportBase {
+public:
+  PLUGININFORMATION("Complete K-partite Graph (OGDF)", "Antoine Lambert", "05/2024",
+                    "Creates the complete k-partite graph K_{k1,k2,...,kn}.", "1.0", "OGDF")
+
+  OGDFCompleteKPartiteGraph(tlp::PluginContext *context) : OGDFImportBase(context) {
+    addInParameter<vector<int>>("signature", paramHelp[0].data(), "(10, 20, 30, 40)");
+  }
+
+  bool importOGDFGraph() override {
+    vector<int> signature = {10, 20, 30, 40};
+    if (dataSet != nullptr) {
+      dataSet->get("signature", signature);
+    }
+    ogdf::completeKPartiteGraph(G, vectorToOGDFArray(signature));
+    return true;
+  }
+};
+
+PLUGIN(OGDFCompleteKPartiteGraph)
---
+++ plugins/import/OGDF/OGDFRandomGraph.cpp
@@ -0,0 +1,53 @@
+/**
+ *
+ * Copyright (C) 2024  The Talipot developers
+ *
+ * Talipot is a fork of Tulip, created by David Auber
+ * and the Tulip development Team from LaBRI, University of Bordeaux
+ *
+ * See the AUTHORS file at the top-level directory of this distribution
+ * License: GNU General Public License version 3, or any later version
+ * See top-level LICENSE file for more information
+ *
+ */
+
+#include <ogdf/basic/graph_generators/randomized.h>
+
+#include "OGDFImportBase.h"
+
+using namespace std;
+
+static constexpr string_view paramHelp[] = {
+    // n
+    "the number of nodes",
+    // m
+    "the number of edges",
+};
+
+//=================================================================================
+
+class OGDFRandomGraph : public OGDFImportBase {
+public:
+  PLUGININFORMATION("Random Graph (OGDF)", "Antoine Lambert", "06/2024", "Creates a random graph",
+                    "1.0", "OGDF")
+
+  OGDFRandomGraph(tlp::PluginContext *context) : OGDFImportBase(context) {
+    addInParameter<int>("n", paramHelp[0].data(), "1000");
+    addInParameter<int>("m", paramHelp[1].data(), "2000");
+  }
+
+  bool importOGDFGraph() override {
+    int n = 1000;
+    int m = 2000;
+
+    if (dataSet != nullptr) {
+      dataSet->get("n", n);
+      dataSet->get("m", m);
+    }
+
+    ogdf::randomGraph(G, n, m);
+    return true;
+  }
+};
+
+PLUGIN(OGDFRandomGraph)
