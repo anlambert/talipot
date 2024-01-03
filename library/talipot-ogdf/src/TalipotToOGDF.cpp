3cdf7708ed98de06a119101f650f3f3c57949e65
Antoine Lambert <antoine.lambert33@gmail.com>
2024-01-03 21:46:42 +0100

    talipot-ogdf/TalipotToOGDF: Fix assertions in debug mode
    Some OGDF layout algorithms require some specific graph attributes
    being available or assertions are raised in debug mode.

--- library/talipot-ogdf/src/TalipotToOGDF.cpp
+++ library/talipot-ogdf/src/TalipotToOGDF.cpp
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright (C) 2019-2023  The Talipot developers
+ * Copyright (C) 2019-2024  The Talipot developers
  *
  * Talipot is a fork of Tulip, created by David Auber
  * and the Tulip development Team from LaBRI, University of Bordeaux
@@ -33,7 +33,10 @@ TalipotToOGDF::TalipotToOGDF(Graph *g, bool importEdgeBends) : talipotGraph(g) {
       // weight field
       ogdf::GraphAttributes::nodeWeight |
       // z coordinate
-      ogdf::GraphAttributes::threeD;
+      ogdf::GraphAttributes::threeD |
+      // required attributes for some OGDF algorithms
+      ogdf::GraphAttributes::nodeStyle | ogdf::GraphAttributes::edgeStyle |
+      ogdf::GraphAttributes::edgeType | ogdf::GraphAttributes::nodeType;
 
   ogdfGraphAttributes = ogdf::GraphAttributes(ogdfGraph, attributes);
 
