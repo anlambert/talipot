b0e9c6ea114f358c0eab5588ae76afaf4c3bd5e7
Antoine Lambert <antoine.lambert33@gmail.com>
2024-05-30 22:31:11 +0200

    talipot-python/bindings: Improve code formatting of some sip files

--- utils/scripts/format-talipot-code.sh
+++ utils/scripts/format-talipot-code.sh
@@ -5,4 +5,4 @@ find ./library/ -iname *.h -o -iname *.cpp -o -iname *.cxx | xargs clang-format 
 find ./plugins/ -iname *.h -o -iname *.cpp -o -iname *.cxx | xargs clang-format -i
 find ./software/ -iname *.h -o -iname *.cpp -o -iname *.cxx | xargs clang-format -i
 find ./tests/ -iname *.h -o -iname *.cpp -o -iname *.cxx | xargs clang-format -i
-find ./library/ -iname *.sip | xargs python3 ./utils/scripts/format_sip_file.py
+find ./library/ -iname *.sip -o -iname *.sip.in | xargs python3 ./utils/scripts/format_sip_file.py
