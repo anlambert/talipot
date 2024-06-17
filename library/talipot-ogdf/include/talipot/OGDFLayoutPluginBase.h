7e15c3de886a8027c18f287de644c849ee345bf4
Antoine Lambert <antoine.lambert33@gmail.com>
2024-06-17 20:39:08 +0200

    talipot-ogdf/OGDFLayoutPluginBase: Use SimpleCCPacker to draw and pack
    Prefer to use SimpleCCPacker layout algorithm instead of ComponentSplitterLayout
    to draw connected components with a given layout algorithm from OGDF and then
    pack the drawings.

--- library/talipot-ogdf/include/talipot/OGDFLayoutPluginBase.h
+++ library/talipot-ogdf/include/talipot/OGDFLayoutPluginBase.h
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright (C) 2019-2021  The Talipot developers
+ * Copyright (C) 2019-2024  The Talipot developers
  *
  * Talipot is a fork of Tulip, created by David Auber
  * and the Tulip development Team from LaBRI, University of Bordeaux
@@ -21,7 +21,7 @@
 namespace ogdf {
 class LayoutModule;
 class GraphAttributes;
-class ComponentSplitterLayout;
+class SimpleCCPacker;
 }
 
 namespace tlp {
@@ -47,7 +47,7 @@ protected:
 
   TalipotToOGDF *tlpToOGDF;
   ogdf::LayoutModule *ogdfLayoutAlgo;
-  ogdf::ComponentSplitterLayout *componentSplitterLayout;
+  ogdf::SimpleCCPacker *simpleCCPacker;
 };
 }
 #endif // TALIPOT_OGDF_LAYOUT_PLUGIN_BASE_H
