anlambert/talipot

commit b70d88e7ee946ec1cc5b4df6b03680b00439a2e0

talipot-python/PythonCodeEditor: Improve selection with Shift+Home
When selecting backwards on a line when using Shift+Home, do not select
indent patterns the first time the Home key is hit.

library/talipot-python/src/PythonCodeEditor.cpp
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright (C) 2019-2023  The Talipot developers
+ * Copyright (C) 2019-2024  The Talipot developers
  *
  * Talipot is a fork of Tulip, created by David Auber
  * and the Tulip development Team from LaBRI, University of Bordeaux
@@ -1060,6 +1060,15 @@ void PythonCodeEditor::keyPressEvent(QKeyEvent *e) {
     getCursorPosition(line, col);
     setSelection(line, col, line, col - _indentPattern.length());
     removeSelectedText();
+  } else if (e->key() == Qt::Key_Home && e->modifiers() == Qt::ShiftModifier) {
+    int line = 0, col = 0;
+    getCursorPosition(line, col);
+    int pos = textBeforeCursor.lastIndexOf(_indentPattern);
+    if (pos != -1 && (pos + _indentPattern.length()) < col) {
+      setSelection(line, pos + _indentPattern.length(), line, col);
+    } else {
+      QPlainTextEdit::keyPressEvent(e);
+    }
   } else {
     QPlainTextEdit::keyPressEvent(e);
 

commit 89f87d039b9f9439101efb3c8ea4274b4807e1c4

talipot-python/PythonCodeEditor: Fix selection regression from b70d88e

library/talipot-python/src/PythonCodeEditor.cpp
@@ -1059,10 +1059,10 @@ void PythonCodeEditor::keyPressEvent(QKeyEvent *e) {
     setSelection(line, col, line, col - _indentPattern.length());
     removeSelectedText();
   } else if (e->key() == Qt::Key_Home && e->modifiers() == Qt::ShiftModifier) {
-    auto [line, col] = getCursorPosition();
+    auto [lineFrom, indexFrom, lineTo, indexTo] = getSelection();
     int pos = textBeforeCursor.lastIndexOf(_indentPattern);
-    if (pos != -1 && (pos + _indentPattern.length()) < col) {
-      setSelection(line, pos + _indentPattern.length(), line, col);
+    if (pos != -1 && (pos + _indentPattern.length()) < indexFrom) {
+      setSelection(lineFrom, pos + _indentPattern.length(), lineTo, indexTo);
     } else {
       QPlainTextEdit::keyPressEvent(e);
     }