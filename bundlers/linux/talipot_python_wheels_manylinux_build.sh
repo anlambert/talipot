c4ff2363fc0a52afb753b88bd97dde21ebc62ebc
Antoine Lambert <antoine.lambert33@gmail.com>
2024-11-26 22:14:15 +0100

    gha/wheels: Remove Python 3.8 build and add those for 3.12 and 3.13
    Python 3.8 reached EOL so there is no need to continue upload wheel
    for it.

--- bundlers/linux/talipot_python_wheels_manylinux_build.sh
+++ bundlers/linux/talipot_python_wheels_manylinux_build.sh
@@ -69,7 +69,8 @@ for CPYBIN in /opt/python/cp*/bin
 do
   if [[ $CPYBIN == *"cp27"* ]] || [[ $CPYBIN == *"cp34"* ]] \
     || [[ $CPYBIN == *"cp35"* ]] || [[ $CPYBIN == *"cp36"* ]] \
-    || [[ $CPYBIN == *"cp37"* ]] || [[ $CPYBIN == *"cp313t"* ]]
+    || [[ $CPYBIN == *"cp37"* ]] || [[ $CPYBIN == *"cp38"* ]] \
+    || [[ $CPYBIN == *"cp313t"* ]]
   then
     continue
   fi
